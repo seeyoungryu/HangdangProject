27a12d77ef1209913511bc7e7d1e8f7329153ca3
liber9194 <kim960928@naver.com>
2023-11-27 14:06:06 +0900

    Update LikeService.java

--- src/main/java/com/example/hangdang/service/LikeService.java
+++ src/main/java/com/example/hangdang/service/LikeService.java
@@ -1,29 +1,75 @@
 package com.example.hangdang.service;
 
-import com.example.hangdang.repository.LikeRepository;
+import com.example.hangdang.dto.GoodsInfoResponseDto;
+import com.example.hangdang.dto.GoodsRequestDto;
+import com.example.hangdang.dto.GoodsResponseDto;
+import com.example.hangdang.entity.Goods;
+import com.example.hangdang.entity.UserEntity;
+import com.example.hangdang.repository.GoodsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class LikeService {
+@RequiredArgsConstructor
+public class GoodsService {
+    private final GoodsRepository goodsRepository;
 
-    private final LikeRepository likeRepository;
 
-    public LikeService(LikeRepository likeRepository) {
-        this.likeRepository = likeRepository;
+    public List<GoodsResponseDto> getGoodsList()
+    {
+        List<GoodsResponseDto> goodsResponseDtoList = new ArrayList<>();
+        List<Goods> goodsList = goodsRepository.findAll();
+
+        for(Goods goods : goodsList)
+        {
+            goodsResponseDtoList.add(new GoodsResponseDto(goods));
+        }
+
+        return goodsResponseDtoList;
     }
 
-    public void like(Long userId, Long goodsId) {
-        // 좋아요 로직 구현
-
+    public void uploadGoods(GoodsRequestDto requestDto, UserEntity user)
+    {
+        Goods goods = new Goods(requestDto, user);
+        goodsRepository.save(goods);
     }
 
+    public void updateGoods(Long goodsId, GoodsRequestDto requestDto, UserEntity user)
+    {
+        Goods goods = goodsRepository.findById(goodsId).orElseThrow(
+                ()-> new NullPointerException("해당하는 게시글이 존재하지 않습니다.")
+        );
 
-    public void unlike(Long userId, Long goodsId) {
-        // 좋아요 취소 로직 구현
+        if(goods.getUser().getNickname().equals(user.getNickname()))
+        {
+            goods.updateGoods(requestDto);
+        }
+
+        goodsRepository.save(goods);
     }
 
+    public void deleteGoods(Long goodsId, UserEntity user)
+    {
+        Goods goods = goodsRepository.findById(goodsId).orElseThrow(
+                ()-> new NullPointerException("해당하는 게시글이 존재하지 않습니다.")
+        );
 
-    public int getTotalLikes(Long goodsId) {
-        return likeRepository.countByGoodsId(goodsId);
+        if(goods.getUser().getNickname().equals(user.getNickname()))
+        {
+            goodsRepository.delete(goods);
+        }
+    }
+
+    public GoodsInfoResponseDto getGoodsInfo(Long goodsId)
+    {
+        Goods goods = goodsRepository.findById(goodsId).orElseThrow(
+                ()-> new NullPointerException("해당하는 게시글이 존재하지 않습니다.")
+        );
+
+        return new GoodsInfoResponseDto(goods);
     }
 }
