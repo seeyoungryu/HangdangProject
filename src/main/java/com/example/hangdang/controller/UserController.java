d5c32405c69d8651f5e7176b94f3a25ab63689e9
seeyoungryu <seeyoungryu@gmail.com>
2023-11-27 21:43:04 +0900

    fix : 회원가입 @valid 추가

--- src/main/java/com/example/hangdang/controller/UserController.java
+++ src/main/java/com/example/hangdang/controller/UserController.java
@@ -5,12 +5,9 @@ import com.example.hangdang.dto.UserInfoResponseDto;
 import com.example.hangdang.dto.UsernameRequestDto;
 import com.example.hangdang.security.UserDetailsImpl;
 import com.example.hangdang.service.UserService;
-import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-    import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -36,21 +33,19 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String signup(@RequestBody SignupRequestDto requestDto) {
+    public String signup(@Valid @RequestBody SignupRequestDto requestDto) {
         userService.signup(requestDto);
 
         return "redirect:/api/login-page";
     }
 
     @GetMapping("/signup/checkName")
-    public String checkUsername(@RequestBody UsernameRequestDto requestDto)
-    {
+    public String checkUsername(@RequestBody UsernameRequestDto requestDto) {
         return userService.checkUsername(requestDto);
     }
 
     @GetMapping("/userinfo/{userId}")
-    public UserInfoResponseDto userInfo(@PathVariable Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails)
-    {
+    public UserInfoResponseDto userInfo(@PathVariable Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return userService.userInfo(userId, userDetails.getUser());
     }
 }
