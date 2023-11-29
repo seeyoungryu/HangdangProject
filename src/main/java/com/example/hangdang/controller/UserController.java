7b265d7993894baf1f92b061c806bb1ecc1acb2f
seeyoungryu <seeyoungryu@gmail.com>
2023-11-29 16:24:46 +0900

    Fix : 회원가입 로그인 기능 코드 수정 및 리팩토링 (UserController,UserService,UserRepository,SignupRequestDto)

--- src/main/java/com/example/hangdang/controller/UserController.java
+++ src/main/java/com/example/hangdang/controller/UserController.java
@@ -2,18 +2,15 @@ package com.example.hangdang.controller;
 
 import com.example.hangdang.dto.SignupRequestDto;
 import com.example.hangdang.dto.UserInfoResponseDto;
-import com.example.hangdang.dto.UsernameRequestDto;
-import com.example.hangdang.security.UserDetailsImpl;
 import com.example.hangdang.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-//@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final UserService userService;
@@ -23,10 +20,6 @@ public class UserController {
         this.userService = userService;
     }
 
-//    @GetMapping("/login-page")
-//    public String loginPage() {
-//        return "login";
-//    }
 
     @GetMapping("/signup")
     public String signupPage() {
@@ -34,23 +27,21 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String signup(@Valid @RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<String> signup(@Valid @RequestBody SignupRequestDto requestDto) {
         userService.signup(requestDto);
-
-        return "redirect:/api/login-page";
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다");
     }
 
-    //    @GetMapping("/signup/check-name")
-//    public String checkUsername(UsernameRequestDto requestDto) {
-//        return userService.checkUsername(requestDto);
-//    }
-    @GetMapping("/check-name")
-    public ResponseEntity<String> checkUsername(@RequestBody UsernameRequestDto requestDto) {
-        return userService.checkUsername(requestDto);
+
+    @GetMapping("/signup/check-name")
+    public ResponseEntity<String> checkUsername(@RequestParam String username) {
+        return userService.checkUsername(username);
     }
+
 
     @GetMapping("/userinfo/{userId}")
-    public UserInfoResponseDto userInfo(@PathVariable Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return userService.userInfo(userId, userDetails.getUser());
+    public UserInfoResponseDto userInfo(@PathVariable Long userId) {
+        return userService.userInfo(userId);
     }
+
 }
--- src/main/java/com/example/hangdang/dto/SignupRequestDto.java
+++ src/main/java/com/example/hangdang/dto/SignupRequestDto.java
@@ -7,27 +7,20 @@ import lombok.Getter;
 
 @Getter
 public class SignupRequestDto {
+
     @NotBlank(message = "아이디를 입력해주세요.")
     @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영문 대소문자와 숫자로만 입력해주세요.")
     @Size(min = 5, max = 12, message = "아이디는 5자 이상 12자 이하로 입력해주세요.")
     private String username;
-
     @NotBlank(message = "비밀번호를 입력해주세요.")
     @Size(min = 5, max = 12, message = "비밀번호는 5자 이상 12자 이하로 입력해주세요.")
     private String password;
-
-
     @NotBlank
     private String nickname;
-
     @NotBlank
     private String phoneNumber;
-
     @NotBlank
     private String address;
-
-    private final boolean admin = false;
-    private final String adminToken = "";
 
     public String getUsername() {
         return username;
@@ -47,13 +40,5 @@ public class SignupRequestDto {
 
     public String getAddress() {
         return address;
-    }
-
-    public boolean isAdmin() {
-        return admin;
-    }
-
-    public String getAdminToken() {
-        return adminToken;
     }
 }
